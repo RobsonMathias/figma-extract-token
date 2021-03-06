b8e67fdea1c1cc55e0e9687c6d5a36123d909cc2
Robson Mathias <robsond.mathias@gmail.com>
2021-03-06 15:47:39 -0300

    docs(github): adding template github pull and issues

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 const { jest: jestConfig } = require('kcd-scripts/config')
 
 module.exports = Object.assign(jestConfig, {
+  testMatch: ['**/__tests__/**/*.test.(js|jsx|ts|tsx)'],
   transform: {
     ...jestConfig.transform,
     '\\.ts?$': 'ts-jest',
