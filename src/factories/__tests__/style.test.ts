91d8e45c3d80819f620fb7230a2e637e59db4d49
Tais Duarte <tais.duarte@creditas.com.br>
2021-04-13 18:33:01 -0300

    fix(drop-shadow): fix drop style to return spread and more than one when is finded

--- src/factories/__tests__/style.test.ts
+++ src/factories/__tests__/style.test.ts
@@ -1,4 +1,4 @@
-import { Paint } from '../../interfaces'
+import { Paint, Effect } from '../../interfaces'
 import { Style } from '../style'
 
 describe('Style', () => {
@@ -17,5 +17,103 @@ describe('Style', () => {
   it('should convert value by unit', () => {
     const when = Style.valueByUnit('12', 'PIXELS')
     expect(when).toEqual('12px')
+  })
+
+  describe('drop shadow', () => {
+    it('shoulds convert drop shadow to style', () => {
+      const given: Effect[] = [
+        {
+          type: 'DROP_SHADOW',
+          visible: true,
+          color: {
+            r: 0.0,
+            g: 0.0,
+            b: 0.0,
+            a: 0.15999999642372131,
+          },
+          blendMode: 'NORMAL',
+          offset: {
+            x: 0.0,
+            y: 16.0,
+          },
+          radius: 12.0,
+          spread: -8.0,
+        },
+        {
+          type: 'DROP_SHADOW',
+          visible: true,
+          color: {
+            r: 0.0,
+            g: 0.0,
+            b: 0.0,
+            a: 0.11999999731779099,
+          },
+          blendMode: 'NORMAL',
+          offset: {
+            x: 0.0,
+            y: 16.0,
+          },
+          radius: 8.0,
+          spread: -12.0,
+        },
+      ]
+
+      const when = Style.effectShadow(given)
+
+      expect(when).toEqual(
+        '0px 16px 12px -8px rgba(0, 0, 0, 0.16), 0px 16px 8px -12px rgba(0, 0, 0, 0.12)',
+      )
+    })
+
+    it('shoulds convert one drop shadow to style', () => {
+      const given: Effect[] = [
+        {
+          type: 'DROP_SHADOW',
+          visible: true,
+          color: {
+            r: 0.0,
+            g: 0.0,
+            b: 0.0,
+            a: 0.15999999642372131,
+          },
+          blendMode: 'NORMAL',
+          offset: {
+            x: 0.0,
+            y: 16.0,
+          },
+          radius: 12.0,
+          spread: -8.0,
+        },
+      ]
+
+      const when = Style.effectShadow(given)
+
+      expect(when).toEqual('0px 16px 12px -8px rgba(0, 0, 0, 0.16)')
+    })
+
+    it('shoulds convert drop shadow to style without spread', () => {
+      const given: Effect[] = [
+        {
+          type: 'DROP_SHADOW',
+          visible: true,
+          color: {
+            r: 0.0,
+            g: 0.0,
+            b: 0.0,
+            a: 0.15999999642372131,
+          },
+          blendMode: 'NORMAL',
+          offset: {
+            x: 0.0,
+            y: 16.0,
+          },
+          radius: 12.0,
+        },
+      ]
+
+      const when = Style.effectShadow(given)
+
+      expect(when).toEqual('0px 16px 12px rgba(0, 0, 0, 0.16)')
+    })
   })
 })
